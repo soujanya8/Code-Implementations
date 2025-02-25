469acea52e3820c03abb48a7477dce950d1bdddb
soujanya8 <soujanyagodugu8@gmail.com>
2025-02-25 12:00:48 -0700

    Temperature Converter

---
+++ Temperatureconv.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Temperatureconv {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double temp;
+        double newTemp;
+        String unit;
+        System.out.println("<<<-------Temperature Conversion ------->>>");
+        System.out.print("Enter the temperature: ");
+        temp = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Is conversion is from Celsius or Fahrenheit (C or F): ");
+        unit = scanner.nextLine().toUpperCase();
+
+        newTemp = (unit.contains("C")) ? 5*(temp-32)/9 : (temp*9/5)+32;
+        
+        System.out.printf("\n\nConverted temperature: %.2f %s\n",newTemp,unit);
+        scanner.close();
+
+    }
+}
--- Weightconverter.java
+++ Weightconverter.java
@@ -5,17 +5,17 @@ public class Weightconverter {
         Scanner scanner = new Scanner(System.in);
         //need to Improve more with the functionality
 
-        System.out.print("Enter the conversion you want to:");
-        String strCon = scanner.nextLine();
+        System.out.print("Convert from kg or lb to lb or kg:");
+        String strCon = scanner.nextLine().toLowerCase();
         System.out.print("Enter the weight:");
         double weightToBeConverted = scanner.nextDouble();
         double weight;
 
-        if(strCon.contains("Kg")||strCon.contains("KG")||strCon.contains("kg") || strCon.contains("Kilo gram") || strCon.contains("Kilogram")){
+        if(strCon.contains("kg") || strCon.contains("kilogram")){
             weight = weightToBeConverted*0.453592 ;
             System.out.printf("\n\n%.2f lb is equal to %.2f kg\n\n",weightToBeConverted, weight);
             
-        }else if(strCon.contains("lb") || strCon.contains("pound") || strCon.contains("Pound")){
+        }else if(strCon.contains("lb") || strCon.contains("pound")){
             weight = weightToBeConverted*2.0462 ;
             System.out.printf("\n\n%.2f Kg is equal to %.2f lb\n\n",weightToBeConverted, weight);
         }
